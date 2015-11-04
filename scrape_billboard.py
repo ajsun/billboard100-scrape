47fa5d7adc5421f89dec618cf848df15e533f38f
Andrew Sun <ajsun@princeton.edu>
2015-11-03 22:10:57 -0500

    new stuff

--- scrape_billboard.py
+++ scrape_billboard.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib2
-import re
+import requests
 import csv
 
 
@@ -8,18 +8,31 @@ baseurl = "http://billboardtop100of.com/"
 
 dates = range(1941,2015)
 
+artists = []
+songs = []
+
 for date in dates:
     url_end = str(date) + "-2/"
     url = baseurl + url_end
     print url
-    response = urllib2.urlopen(url)
-    html = response.read()
+    temp_artists = []
+    temp_songs = []
+    response = requests.get(url)
+    html = response.text
     soup = BeautifulSoup(html, "lxml")
     try:
-        table = soup.find(class_="alignleft")
+        table = soup.find("table")
         strings = table.find_all('td')
+        count = 1
         for i in strings:
-            print i.string
+            if (count % 3 == 2):
+                temp_artists.append(i.string)
+            elif (count % 3 == 0):
+                temp_songs.append(i.string)
+            count = count + 1
+
     except:
         print "Error: " + str(date)
+    artists.append(temp_artists)
+    songs.append(temp_songs)
         
